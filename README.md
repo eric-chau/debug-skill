08ec50c5cda5a43dbab5917dd9f4b0b894c951a1
Eric Chau <eriic.chau@gmail.com>
2015-09-05 23:48:26 +0000

    initialized Debug Skill to integrate Symfony Debug component into Jarvis

---
+++ README.md
@@ -0,0 +1,5 @@
+# Debug skill
+
+The debug skill is an integration of `Symfony Debug` into Jarvis micro-framework.
+
+Note that it ONLY works on `debug: true` mode.
---
+++ src/ContainerProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Jarvis\Skill\Debug;
+
+use Jarvis\Jarvis;
+use Jarvis\Skill\DependencyInjection\ContainerProviderInterface;
+use Jarvis\Skill\EventBroadcaster\JarvisEvents;
+use Jarvis\Skill\EventBroadcaster\ExceptionEvent;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\Debug\ExceptionHandler;
+
+/**
+ * @author Eric Chau <eric.chau@gmail.com>
+ */
+class ContainerProvider implements ContainerProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function hydrate(Jarvis $jarvis)
+    {
+        if ($jarvis->debug) {
+            Debug::enable();
+
+            $jarvis->addReceiver(JarvisEvents::EXCEPTION_EVENT, function (ExceptionEvent $event) {
+                $event->setResponse((new ExceptionHandler())->createResponse($event->getException));
+            });
+        }
+    }
+}
